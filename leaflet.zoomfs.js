1e8a2048a7954ed6de05a5762e0101f227ff95ee
Tom MacWright <tom@macwright.org>
2013-02-15 14:45:24 -0500

    Match leaflet code style, fix jshint issues.

--- leaflet.zoomfs.js
+++ leaflet.zoomfs.js
@@ -4,10 +4,10 @@
  * https://github.com/elidupuis/leaflet.zoomfs
  */
 L.Control.ZoomFS = L.Control.Zoom.extend({
-  includes: L.Mixin.Events,
-  onAdd: function (map) {
+	includes: L.Mixin.Events,
+	onAdd: function (map) {
 		var className = 'leaflet-control-zoom',
-		    container = L.DomUtil.create('div', className);
+		container = L.DomUtil.create('div', className);
 
 		this._map = map;
 		this._isFullscreen = false;
@@ -20,63 +20,63 @@ L.Control.ZoomFS = L.Control.Zoom.extend({
 		return container;
 
 	},
-  fullscreen: function(){
-    // call appropriate internal function
-	  if (!this._isFullscreen) {
-	    this._enterFullScreen();
-	  }else{
-	    this._exitFullScreen();
-	  };
+	fullscreen: function() {
+		// call appropriate internal function
+		if (!this._isFullscreen) {
+			this._enterFullScreen();
+		} else {
+			this._exitFullScreen();
+		}
 
-    // force internal resize
-	  this._map.invalidateSize();
-  },
-  _enterFullScreen: function(){
-    var container = this._map._container;
+		// force internal resize
+		this._map.invalidateSize();
+	},
+	_enterFullScreen: function() {
+		var container = this._map._container;
 
-    // apply our fullscreen settings
-    container.style.position = 'fixed';
+		// apply our fullscreen settings
+		container.style.position = 'fixed';
 		container.style.left = 0;
 		container.style.top = 0;
 		container.style.width = '100%';
 		container.style.height = '100%';
 
-    // store state
-    L.DomUtil.addClass(container, 'leaflet-fullscreen');
-	  this._isFullscreen = true;
+		// store state
+		L.DomUtil.addClass(container, 'leaflet-fullscreen');
+		this._isFullscreen = true;
 
-	  // add ESC listener
-	  L.DomEvent.addListener(document, 'keyup', this._onKeyUp, this);
-	  
-    // fire fullscreen event on map
-    this._map.fire('enterFullscreen');
-  },
-  _exitFullScreen: function(){
-    var container = this._map._container;
+		// add ESC listener
+		L.DomEvent.addListener(document, 'keyup', this._onKeyUp, this);
 
-    // update state
-    L.DomUtil.removeClass(container, 'leaflet-fullscreen');
-    this._isFullscreen = false;
+		// fire fullscreen event on map
+		this._map.fire('enterFullscreen');
+	},
+	_exitFullScreen: function() {
+		var container = this._map._container;
 
-    // remove fullscreen style; make sure we're still position relative for Leaflet core.
-    container.removeAttribute('style');
-    
-    // re-apply position:relative; if user does not have it.
-    var position = L.DomUtil.getStyle(container, 'position');
-    if (position !== 'absolute' && position !== 'relative') {
-     container.style.position = 'relative';
-    }
+		// update state
+		L.DomUtil.removeClass(container, 'leaflet-fullscreen');
+		this._isFullscreen = false;
 
-    // remove ESC listener
-	  L.DomEvent.removeListener(document, 'keyup', this._onKeyUp);
+		// remove fullscreen style; make sure we're still position relative for Leaflet core.
+		container.removeAttribute('style');
 
-    // fire fullscreen event
-    this._map.fire('exitFullscreen');
-  },
-  _onKeyUp: function(e){
-    if (!e) var e = window.event;
-    if (e.keyCode === 27 && this._isFullscreen === true) {
-      this._exitFullScreen();
-    }
-  }
+		// re-apply position:relative; if user does not have it.
+		var position = L.DomUtil.getStyle(container, 'position');
+		if (position !== 'absolute' && position !== 'relative') {
+			container.style.position = 'relative';
+		}
+
+		// remove ESC listener
+		L.DomEvent.removeListener(document, 'keyup', this._onKeyUp);
+
+		// fire fullscreen event
+		this._map.fire('exitFullscreen');
+	},
+	_onKeyUp: function(e) {
+		if (!e) e = window.event;
+		if (e.keyCode === 27 && this._isFullscreen === true) {
+			this._exitFullScreen();
+		}
+	}
 });
